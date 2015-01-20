ed7a82d104530d9fda098647e54c9957ec85e142
ihuijsmans <ihuijsmans@hotmail.com>
2015-01-20 15:54:45 +0100

    Updated HRlogs from Bidding Task to Shop Task

--- ShopTaskHRlogs.m
+++ ShopTaskHRlogs.m
@@ -8,10 +8,10 @@
 %Trial logs (scarcity first)
 c.HR_fixcross = {71,91};
 c.HR_image = {72,92};
-c.HR_startbid = {73,93};
-c.HR_response = {74,94};
+c.HR_price = {73,93};
+c.HR_yesno = {74,94};
 c.HR_gotnot = {[75,76],[95,96]}; %[S_got, S_not],[A_got, A_not] 
-c.HR_moneyspent = {77,97};
+c.HR_wrong = {77,97};
 c.HR_slow = {78,98};
 c.HR_quit = {79,99};
 
